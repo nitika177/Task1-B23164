580ed6ce00308a5f16953bfe4619648d9c612e08
nitika177 <nitika177@gmail.com>
2024-07-08 21:36:06 +0530

    this is task 1

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/nitika/ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH='/home/nitika/ws/devel/lib:/opt/ros/noetic/lib:/opt/ros/noetic/lib/x86_64-linux-gnu:/home/nitika/catkin_ws/devel/lib'
+export PKG_CONFIG_PATH='/home/nitika/ws/devel/lib/pkgconfig:/opt/ros/noetic/lib/pkgconfig:/opt/ros/noetic/lib/x86_64-linux-gnu/pkgconfig'
+export PWD='/home/nitika/ws/build'
+export PYTHONPATH="/home/nitika/ws/devel/lib/python3/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES='/home/nitika/ws/devel/share/common-lisp:/home/nitika/catkin_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH='/home/nitika/ws/src:/opt/ros/noetic/share:/home/nitika/catkin_ws/src'
---
+++ build/turtlebot3_example/CMakeFiles/turtlebot3_example_generate_messages_py.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "/home/nitika/ws/devel/lib/python3/dist-packages/turtlebot3_example/msg/_Turtlebot3Action.py"
+  "/home/nitika/ws/devel/lib/python3/dist-packages/turtlebot3_example/msg/_Turtlebot3ActionFeedback.py"
+  "/home/nitika/ws/devel/lib/python3/dist-packages/turtlebot3_example/msg/_Turtlebot3ActionGoal.py"
+  "/home/nitika/ws/devel/lib/python3/dist-packages/turtlebot3_example/msg/_Turtlebot3ActionResult.py"
+  "/home/nitika/ws/devel/lib/python3/dist-packages/turtlebot3_example/msg/_Turtlebot3Feedback.py"
+  "/home/nitika/ws/devel/lib/python3/dist-packages/turtlebot3_example/msg/_Turtlebot3Goal.py"
+  "/home/nitika/ws/devel/lib/python3/dist-packages/turtlebot3_example/msg/_Turtlebot3Result.py"
+  "/home/nitika/ws/devel/lib/python3/dist-packages/turtlebot3_example/msg/__init__.py"
+  "CMakeFiles/turtlebot3_example_generate_messages_py"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/turtlebot3_example_generate_messages_py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
